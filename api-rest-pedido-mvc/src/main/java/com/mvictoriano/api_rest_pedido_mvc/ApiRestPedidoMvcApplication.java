2c441ca2c6340cf900801678a7c6e9dd344ba6a5
Michelle Gaia Victoriano <mgvictoriano@live.com>
2025-06-22 11:54:56 -0300

    Add files via upload
    initial commit

---
+++ api-rest-pedido-mvc/src/main/java/com/mvictoriano/api_rest_pedido_mvc/ApiRestPedidoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mvictoriano.api_rest_pedido_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPedidoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPedidoMvcApplication.class, args);
+	}
+
+}
---
+++ api-rest-pedido-mvc/src/test/java/com/mvictoriano/api_rest_pedido_mvc/ApiRestPedidoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvictoriano.api_rest_pedido_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPedidoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
