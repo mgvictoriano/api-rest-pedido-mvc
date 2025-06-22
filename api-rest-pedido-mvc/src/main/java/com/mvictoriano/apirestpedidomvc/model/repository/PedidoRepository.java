a80989c2ab0d8de538798b7deaebebd8aa276459
Michelle Victoriano <michelle.victoriano@attus.ai>
2025-06-22 13:03:14 -0300

    feat: add imports

--- api-rest-pedido-mvc/src/main/java/com/mvictoriano/apirestpedidomvc/model/repository/PedidoRepository.java
+++ api-rest-pedido-mvc/src/main/java/com/mvictoriano/apirestpedidomvc/model/repository/PedidoRepository.java
@@ -1,3 +1,13 @@
+package com.mvictoriano.apirestpedidomvc.model.repository;
+
+import com.mvictoriano.apirestpedidomvc.model.domains.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByDescricaoContainingIgnoreCase(String descricao);
+    List<Pedido> findByClienteContainingIgnoreCase(String cliente);
+    List<Pedido> findByStatus(String status);
 }
